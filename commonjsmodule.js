be46fa6112720d1e6b990918630b8542257c3b58
Dustin Machi <dmachi@dojotoolkit.org>
2010-04-10 01:10:36 -0400

    give packages a default search path for engines

--- commonjsmodule.js
+++ commonjsmodule.js
@@ -1,52 +1,88 @@
 (function(){
 	var basePath = Titanium.Filesystem.getResourcesDirectory() + "/CommonJS";
 	var modules = {};
-	var paths = [basePath];
+
+	// paths to packages
+	var paths = [basePath]
+
+	// paths to search within packages
+	var subSearchPaths = ["engines/titanium/lib","engines/default/lib","lib"]
 
 	function resolveId(currentId, id){
-		//Titanium.API.debug("resolveUrl: " + currentId + " " + id);
+
+		//relative request
 		if(id.charAt(0) === '.'){
-			currentId = currentId.substring(0, currentId.lastIndexOf('/') + 1);
-			return (currentId + id ).replace(/\/\.\.\/[^\/]*/g,'').replace(/\.\//g,'');
+			//Titanium.API.debug("   Relative request:" +  currentId.substring(0, currentId.lastIndexOf('/') + 1) + " :: " + id);
+			var currentParts = currentId.split("/");
+			var idParts = id.split("/");
+			var finalParts = [];
+			for (var i=0; i<idParts.length;i++){
+				if (idParts[i]=="..") {
+					currentParts.pop();
+				}else{
+					finalParts.push(idParts[i]);
+				}
+			}
+			currentId = currentParts.join("/");
+			id = finalParts.join("/");
+			currentId = currentId.substring(0, currentId.lastIndexOf('/') ) || currentId;
+			return (currentId + "/" + id ).replace(/\/\.\.\/[^\/]*/g,'').replace(/\.\//g,'');
 		}
-		var idParts = id.split("/");
-		if (idParts.length==1) {  
-			idParts.push("lib");
-			idParts.push(idParts[0]);	
-		}else{
-			idParts.splice(1,0, "lib");
-			return currentId + "/" + idParts.join("/");
-		}
-		idParts.unshift(currentId)
-		var id = idParts.join("/");
-		//Titanium.API.debug("id: " + id );
 		return id;
 	}
 
 	function makeRequire(currentId){
 		var require = function(id){
-			//Titanium.API.debug("require(" + id + ")");
 			id = resolveId(currentId, id);
-			//Titanium.API.debug("Resolved ID: "+ id);
 			if(modules[id]){
 				return modules[id];
 			}
 			var exports = modules[id] = {};
 			var file = null;
 			var path=paths.some(function(path){
-				var name = path + id + ".js";	
-				//Titanium.API.debug("Searching for file: " + name);
-				var f = Titanium.Filesystem.getFile(name);
-				if (f.isFile()) {	
-					file = f;
-					return path;
+				var idParts = id.split("/");
+				if (idParts.length==1) {  
+					var initialName = path + "/" + id;
+					var initial = Titanium.Filesystem.getFile(initialName);
+					if (initial.isDirectory()) {
+						//TODO IMPORT package.json initialName + /package.json
+						idParts.push(subSearchPaths[0]);
+						idParts.push(idParts[0]);	
+					}else{
+						var idPath = idParts.join("/");
+						var name = path + "/" + idPath + ".js";	
+						var f = Titanium.Filesystem.getFile(name);
+						if (f.isFile()) {	
+							file = f;
+							return path;
+						}
+						Titanium.API.error("Unable to locate resource: " + id);	
+						return;
+					}
+
+				}else{
+					idParts.splice(1,0, subSearchPaths[0]);
 				}
+
+				
+				for (var i=0; i<subSearchPaths.length;i++) {	
+					
+					idParts[1]=subSearchPaths[i];
+					var idPath = idParts.join("/");
+
+					var name = path + "/" + idPath + ".js";	
+					Titanium.API.debug("Searching for file: " + name);
+					var f = Titanium.Filesystem.getFile(name);
+					if (f.isFile()) {	
+						file = f;
+						return path;
+					}
+				}
+
 			});
 
 			if (path && file){ 
-				//Titanium.API.debug("Reading File: " + file);
         			var source = file.read();
-				//Titanium.API.debug("Source: " + source);
 				var factory = eval("(function(require, exports, module){" + source + "})"); 
 				factory(makeRequire(id), exports, {id:id});
 				return exports;
